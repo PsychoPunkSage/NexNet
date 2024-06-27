26b02dc83fce7cde81c29b08d99d3e5285215cd7
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-27 16:20:50 +0530

    fix::> Working on Node communication

---
+++ p2p/transport.go
@@ -0,0 +1,8 @@
+package p2p
+
+// Peer: Representation of remote node.
+type Peer interface{}
+
+// Transport: Anything that handle communication between node in the Network.
+// This can be of form TCP, UDP, websockets, etc.
+type Transport interface{}
