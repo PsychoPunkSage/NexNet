5e41ac9c2ca58e6a16122a531fca1029943e4711
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-29 14:30:01 +0530

    fix::> Enabled NOPHandshake for testing

--- p2p/handshake.go
+++ p2p/handshake.go
@@ -1,5 +1,11 @@
 package p2p
 
-type Handshaker interface {
-	Handshake() error
-}
+import "errors"
+
+// ErrInvalidHandShake : returned if handshake between local and remote node couldn't be established.
+var ErrInvalidHandShake = errors.New("invalid handshake")
+
+// HandshakeFunc:
+type HandshakeFunc func(Peer) error
+
+func NOPHandshakeFunc(Peer) error { return nil }
