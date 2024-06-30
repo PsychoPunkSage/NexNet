3cec92c0f5812ff8bfb2208384f3b127adc81224
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-30 19:34:45 +0530

    fix::> updated main.go to incorporate storage and onPeer func test

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"github.com/PsychoPunkSage/NexNet/p2p"
 )
 
-func OnPeer(p2p.Peer) error {
+func OnPeer(peer p2p.Peer) error {
+	peer.Close()
 	fmt.Println("Doing some logic with the peer outside of TCPTransport")
 	return nil
 }
