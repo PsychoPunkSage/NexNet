cd55e3f1eddf27735127b89f96f8c0d47965d923
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-01 12:03:08 +0530

    fix::> Able to create a hashed filesystem using HASHing and keys

--- store.go
+++ store.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 type PathTransformFunc func(string) string
 
 var DefaultPathTransformFunc = func(key string) string {
 	return key
+}
+
+func CASPathTransformFunc(key string) string {
+	hash := sha1.Sum([]byte(key))
+	fmt.Println("Hash: ", hash)
+	hashStr := hex.EncodeToString(hash[:])
+	fmt.Println("Hash String: ", hashStr)
+
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+	fmt.Println("sliceLen: ", sliceLen)
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, i*blocksize+blocksize
+		paths[i] = hashStr[from:to]
+	}
+
+	return strings.Join(paths, "/")
 }
 
 type StoreOpts struct {
@@ -32,9 +57,12 @@ func (s *Store) writeStream(r io.Reader, key string) error {
 		return err
 	}
 
-	filename := "Somefilename"
-
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r)
+	filenameBytes := md5.Sum(buf.Bytes())
+	filename := hex.EncodeToString(filenameBytes[:])
 	pathAndFilename := pathName + "/" + filename
+
 	f, err := os.Create(pathAndFilename)
 	if err != nil {
 		return err
