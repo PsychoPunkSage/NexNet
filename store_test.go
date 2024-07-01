ffd4eae7310c8c0ae60bb9587ec3f8de416bef0c
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-01 12:03:36 +0530

    fix::> Wrote tests for required functionality

--- store_test.go
+++ store_test.go
@@ -3,19 +3,26 @@ package main
 import (
 	"bytes"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		pathTransformFunc: DefaultPathTransformFunc,
+		pathTransformFunc: CASPathTransformFunc,
 	}
 	store := NewStream(opts)
-	assert.Equal(t, store.pathTransformFunc("AP"), "AP")
 
 	data := bytes.NewReader([]byte("some png data"))
 	if err := store.writeStream(data, "myspecialpic"); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestPathTransformFunc(t *testing.T) {
+	key := "mybestpic"
+	pathname := CASPathTransformFunc(key)
+	// fmt.Println(pathname)
+	expectedPathname := "1b150/aae86/eedae/268f6/589f4/0fb48/b2a0d/47ff4"
+	if pathname != expectedPathname {
+		t.Error(t, "Want %s Got %s", expectedPathname, pathname)
 	}
 }
