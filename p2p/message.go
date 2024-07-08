00de6996b1e1566f77c591921f84a973af15c91d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-08 12:58:08 +0530

    fix::> Added Stream to RPC interface

--- p2p/message.go
+++ p2p/message.go
@@ -2,8 +2,14 @@ package p2p
 
 import "net"
 
+const (
+	IncomingMessage = 0x1
+	IncomingStream  = 0x2
+)
+
 // RPC : arbitrary data that is being sent over each transport between 2 node (peer) in the network
 type RPC struct {
 	Payload []byte
 	From    net.Addr
+	Stream  bool
 }
